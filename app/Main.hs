884e599a4b179e07024fce69e0e82be4a50094b3
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-04-30 16:12:31 +0200

    Introduce global error type

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,15 @@ import Control.Monad.Except
 import Control.Monad.Trans.Except
 import Node
 
+data AppError = AppCPError CPError | AppNodeError NodeError
+
 main :: IO ()
 main = do
   Right nodes <- runExceptT $ readNodes "app.conf"
   putStrLn (show nodes)
 
-readNodes :: String -> ExceptT CPError IO [Node]
+readNodes :: String -> ExceptT AppError IO [Node]
 readNodes configFile =  do
-  conf <- join $ liftIO $ readfile emptyCP configFile
-  hosts <- get conf "DEFAULT" "hosts"
+  conf <- withExceptT AppCPError $ join $ liftIO $ readfile emptyCP configFile
+  hosts <- withExceptT AppCPError $ get conf "DEFAULT" "hosts"
   return $ parseNodeAddresses hosts
