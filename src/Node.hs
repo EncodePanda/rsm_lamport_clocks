47bcb20ae21aa9cc9a3ff36e145eaee4479489e4
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-04-30 18:09:33 +0200

    Use NonEmpty instead of List
    Add tests for the Node module

--- src/Node.hs
+++ src/Node.hs
@@ -1,17 +1,17 @@
 module Node where
 
 import Data.List.Split
+import Data.List.NonEmpty
 
-data NodeError = NodeAddressNotParsable String
+data NodeError = NodeAddressNotParsable String deriving (Show, Eq)
 
 type Host = String
 type Port = Int
 data Node = Node Host Port
-  deriving Show
+  deriving (Show, Eq)
 
-parseNodeAddresses :: String -> Either NodeError [Node]
-parseNodeAddresses =  traverse (toNode . (splitOn ":")) . (splitOn ",")
+parseNodeAddresses :: String -> Either NodeError (NonEmpty Node)
+parseNodeAddresses =  fmap fromList . traverse (toNode . (splitOn ":")) . (splitOn ",")
   where
     toNode (host:port:[]) = Right $ Node host (read port)
     toNode line = Left $ NodeAddressNotParsable $ show line
-
