b999d4ed0fbcac71f50d973172c0d711e0a1ab58
Pawel Szulc <paul.szulc@gmail.com>
2019-05-04 18:34:49 +0200

    Change client to take arguments on startup
    serverAddr represents server address to which client will send a
    command
    clientAddr represents client address on which it listents to responses
    from the server
    command is the string representation of the command being send to the server

--- client/Main.hs
+++ client/Main.hs
@@ -1,4 +1,34 @@
 module Main where
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+import Node
+
+
+data Arg = Arg String String String
+           deriving Show
+
 main :: IO ()
-main = putStrLn "This is client"
+main = send =<< execParser opts
+  where
+    opts = info (parseArgs <**> helper)
+      ( fullDesc
+     <> progDesc "Client sending commands to the node"
+     <> header "rsm-client" )
+
+    send = putStrLn . show
+
+parseArgs :: Parser Arg
+parseArgs = Arg
+      <$> strOption
+          ( long "serverAddr"
+         <> metavar "HOST:PORT"
+         <> help "represents server address to which client will send a command" )
+      <*> strOption
+          ( long "clientAddr"
+         <> metavar "HOST:PORT"
+         <> help "represents client address on which it listents to responses" )
+      <*> strOption
+          ( long "command"
+         <> metavar "COMMAND"
+         <> help "string representation of the command being send to the server" )          
