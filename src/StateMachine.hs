34e01018f7cb307dc09e60885557beecabd5c9b7
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-05-30 09:33:00 +0200

    Add serialize, deserialize definitions

--- src/StateMachine.hs
+++ src/StateMachine.hs
@@ -1,6 +1,7 @@
 module StateMachine where
 
 import Control.Monad.State
+import Data.ByteString
 
 data Command = Add Int | Mult Int deriving Show
 
@@ -9,3 +10,9 @@ type TheState = Int
 machine :: Command -> State TheState ()
 machine (Add x)  = modify (+x)
 machine (Mult x) = modify (*x)
+
+serialize :: Command -> ByteString
+serialize c = undefined
+
+deserialize :: ByteString -> Command
+deserialize c = undefined
