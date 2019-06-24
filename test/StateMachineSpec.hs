37a60d9e75f1cafcdee69e73abee71f3ceb41b55
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-06-24 19:33:37 +0200

    Property tested tick

--- test/StateMachineSpec.hs
+++ test/StateMachineSpec.hs
@@ -1,3 +1,4 @@
+
 module StateMachineSpec where
 
 import StateMachine
@@ -19,16 +20,11 @@ spec = describe "machine" $ do
       property (\event -> (deserialize . serialize $ event) `shouldBe` event)
 
   describe "tick" $ do
-    it "sets current time greater than incoming external command's Tm" $ do
-      let currentTime = 3
-      (execState (tick (ExternalCommand 5 (Add 2))) currentTime) `shouldBe` 6
     it "sets current time greater or equal current value if incoming external command's Tm is smaller" $ do
-      let currentTime = 7
-      (execState (tick (ExternalCommand 5 (Add 2))) 7) `shouldBe` 7
+      forAll externalCommandGen (\extCmd @ (ExternalCommand externalClock cmd) currentTime -> execState (tick extCmd) currentTime `shouldBe` if(externalClock < currentTime) then currentTime else externalClock + 1)
     it "bumps the clock by 1 if incoming command is internal" $ do
       let currentTime = 2
-      (execState (tick (InternalCommand (Add 2))) 7) `shouldBe` 8
-
+      forAll internalCommandGen (\cmd -> execState (tick cmd) 1 `shouldBe` 2)
 
 apply :: [Command] -> State TheState [()]
 apply cmds = traverse machine cmds
@@ -40,3 +36,15 @@ instance Arbitrary Command where
   arbitrary = do
     i <- choose(0, 100)
     elements[Add i, Mult i]
+
+internalCommandGen = do
+    cmd <- arbitrary
+    elements[(InternalCommand cmd)]
+
+externalCommandGen = do
+    clock <- arbitrary
+    cmd <- arbitrary
+    elements[(ExternalCommand clock cmd)]
+
+instance Arbitrary IncomingCommand where
+  arbitrary = oneof [internalCommandGen, externalCommandGen]
