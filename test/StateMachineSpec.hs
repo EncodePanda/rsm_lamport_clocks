07065b5923199c2133d17667c5a9601202055126
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-07-02 12:18:46 +0200

    Tweak property tests for external commands

--- test/StateMachineSpec.hs
+++ test/StateMachineSpec.hs
@@ -20,10 +20,15 @@ spec = describe "machine" $ do
       property (\event -> (deserialize . serialize $ event) `shouldBe` event)
 
   describe "tick" $ do
-    it "sets current time greater or equal current value if incoming external command's Tm is smaller" $ do
-      forAll externalCommandGen (\extCmd @ (ExternalCommand externalClock cmd) currentTime -> execState (tick extCmd) currentTime `shouldBe` if(externalClock < currentTime) then currentTime else externalClock + 1)
+    it "sets current time greater or equal current value and greater than max of highest incoming" $ do
+      forAll externalCommandsAndClockGen (\(clock, cmds) -> execState (sequence ( map tick cmds )) clock `shouldSatisfy` (\result -> result >= clock && result >= maxClock cmds))
     it "the clock is bumped by 1 every time an internal incoming command is encountered" $ do
       forAll internalCommandsAndClockGen (\(clock, cmds) -> execState (sequence ( map tick cmds )) clock `shouldBe` clock + length cmds)
+
+maxClock :: [IncomingCommand] -> Int
+maxClock [] = 0
+maxClock ((ExternalCommand clock _):cons) = max (maxClock cons) clock
+maxClock ((InternalCommand _):cons) = maxClock cons
 
 apply :: [Command] -> State TheState [()]
 apply cmds = traverse machine cmds
@@ -35,6 +40,11 @@ instance Arbitrary Command where
   arbitrary = do
     i <- choose(0, 100)
     elements[Add i, Mult i]
+
+externalCommandsAndClockGen :: Gen (Int, [IncomingCommand])
+externalCommandsAndClockGen = do
+  clock <- choose(0, 100)
+  suchThatMap (listOf1 externalCommandGen) (\c -> Just(clock, c))
 
 internalCommandsAndClockGen :: Gen (Int, [IncomingCommand])
 internalCommandsAndClockGen = do
